bba29905fa3f3e523ea9454503b64eb11ad7b1ff
Rajnish Dubey <rajdubey76890@gmail.com>
2026-08-26 11:10:09 +0530

    feature: added for multiple course for same student

--- src/routes/admissionRoutes.js
+++ src/routes/admissionRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {
   createAdmission,
+  searchStudentsWithAdmissions,
   getAdmissions,
   getAdmissionById
 } = require('../controllers/admissionController');
 const { protect, authorize } = require('../middleware/auth');
 
+router.get('/student/search', protect, authorize('admin', 'receptionist'), searchStudentsWithAdmissions);
 router.post('/', protect, authorize('admin', 'receptionist'), createAdmission);
 router.get('/', protect, authorize('admin', 'receptionist'), getAdmissions);
 router.get('/:id', protect, getAdmissionById);
