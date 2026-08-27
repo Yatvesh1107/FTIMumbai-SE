44916b079f95d4023ad3b342559cf58ab30c2648
Rajnish Dubey <rajdubey76890@gmail.com>
2026-08-27 14:34:58 +0530

    done

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { login, getMe, registerUser, getUsers } = require('../controllers/authController');
+const { login, getMe, registerUser, getUsers, deleteUser } = require('../controllers/authController');
 const { protect, authorize } = require('../middleware/auth');
 
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.post('/register', protect, authorize('admin'), registerUser);
 router.get('/users', protect, authorize('admin'), getUsers);
+router.delete('/users/:id', protect, authorize('admin'), deleteUser);
 
 module.exports = router;
